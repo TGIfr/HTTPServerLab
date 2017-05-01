dfa8c85cfdb23b095532930124ac8b99ff931214
TGIfr <mihail.lukjanec@gmail.com>
2017-05-01 22:52:18 +0300

    travis test commit 2

--- scripts/install-libprogbase.sh
+++ scripts/install-libprogbase.sh
@@ -4,3 +4,8 @@ wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase/archive/ma
 unzip libprogbase.zip -d libprogbase
 cd libprogbase/libprogbase-master ; sudo make install
 cd ../../ ; rm libprogbase/ libprogbase.zip -rf
+
+wget -O libprogbase-cpp.zip https://github.com/PublicHadyniak/libprogbase-cpp/archive/master.zip
+unzip libprogbase-cpp.zip -d libprogbase-cpp
+cd libprogbase-cpp/libprogbase-master ; cmake ; sudo make install
+cd ../../ ; rm libprogbase/ libprogbase.zip -rf
